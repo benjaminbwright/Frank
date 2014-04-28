dc8cfb4fb39311dde18da7a8bc88b3a4ac2bd93c
Benjamin Wright <benajmin.b.wright@gmail.com>
2014-04-28 11:12:15 -0400

    Added Index file

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+
+/**
+ * Frank: A simple PHP5 MVC framework.
+ *
+ * @package    Frank
+ * @version    0.1.0
+ * @author     Benjamin Wright
+ * @license    MIT License
+ * @copyright  2014 Benjamin Wright
+ * @link       http://www.benjaminbwright.com
+ */
